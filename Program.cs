04e3409584b4cec8e71dd5c9972a742b0aa5170a
DFI-named-ION <larityandselnter@gmail.com>
2022-12-04 01:47:36 +0200

    Project zip move

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace Calculator
     {
         static void Main()
         {
-            Console.WriteLine("Gello");
+            MenuManager menuManager = new MenuManager();
+            CalcManager calcManager = new CalcManager();
+            DialogManager dialogManager = new DialogManager();
+
+            do
+            {
+                Console.WriteLine();
+                menuManager.Display();
+
+                switch (menuManager.GetChoice())
+                {
+                    case 1:
+                        dialogManager.AddDialog();
+                        break;
+                    case 2:
+                        dialogManager.SubDialog();
+                        break;
+                    case 3:
+                        dialogManager.MulDialog();
+                        break;
+                    case 4:
+                        dialogManager.DivDialog();
+                        break;
+                }
+            } while (menuManager.AllowContinue());
         }
     }
 }
